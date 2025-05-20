994ec6c1bd5aa982082a08afe2a84c3c4678d139
MrAlmondNut <roosterwade56@gmail.com>
2025-05-20 17:09:16 +0700

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,29 +1,103 @@
 package model;
+public class Product {
+    private String proId;
+    private String proModel;
+    private String proCategory;
+    private String proName;
+    private double proCurrentPrice;
+    private double proRawPrice;
+    private double proDiscount;
+    private int proLikesCount;
 
-public  class Users {
-    protected String userid;
-    protected String username;
-    protected String userpassword;
-    protected String userRegisterTime;
-    protected String userRole;
-    public Users (String userid, String username, String userpassword, String userRole,String userRegisterTime){
-    this.userid = userid;
-    this.username = username;
-    this.userpassword = userpassword;
-    this.userRegisterTime = userRegisterTime;
-    this.userRole = userRole;
-    
-    
-    
+    // Full constructor
+    public Product(String proId, String proModel, String proCategory,
+                   String proName, double proCurrentPrice, double proRawPrice,
+                   double proDiscount, int proLikesCount) {
+        this.proId = proId;
+        this.proModel = proModel;
+        this.proCategory = proCategory;
+        this.proName = proName;
+        this.proCurrentPrice = proCurrentPrice;
+        this.proRawPrice = proRawPrice;
+        this.proDiscount = proDiscount;
+        this.proLikesCount = proLikesCount;
+    }
 
-}
+    // Default constructor
+    public Product() {
+        this("p_00000", "defaultModel", "defaultCategory", "defaultName",
+             0.0, 0.0, 0.0, 0);
+    }
 
-    
+    public String getProId() {
+        return proId;
+    }
+
+    public String getProModel() {
+        return proModel;
+    }
+
+    public String getProCategory() {
+        return proCategory;
+    }
+
+    public String getProName() {
+        return proName;
+    }
+
+    public double getProCurrentPrice() {
+        return proCurrentPrice;
+    }
+
+    public double getProRawPrice() {
+        return proRawPrice;
+    }
+
+    public double getProDiscount() {
+        return proDiscount;
+    }
+
+    public int getProLikesCount() {
+        return proLikesCount;
+    }
+
+    public void setProId(String proId) {
+        this.proId = proId;
+    }
+
+    public void setProModel(String proModel) {
+        this.proModel = proModel;
+    }
+
+    public void setProCategory(String proCategory) {
+        this.proCategory = proCategory;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+
+    public void setProCurrentPrice(double proCurrentPrice) {
+        this.proCurrentPrice = proCurrentPrice;
+    }
+
+    public void setProRawPrice(double proRawPrice) {
+        this.proRawPrice = proRawPrice;
+    }
+
+    public void setProDiscount(double proDiscount) {
+        this.proDiscount = proDiscount;
+    }
+
+    public void setProLikesCount(int proLikesCount) {
+        this.proLikesCount = proLikesCount;
+    }
+
     @Override
     public String toString() {
-        return String.format(
-            "{\"user_id\":\"%s\", \"user_name\":\"%s\", \"user_password\":\"%s\", \"user_register_time\":\"%s\", \"user_role\":\"%s\"}",
-            userid, username, userpassword, userRegisterTime, userRole
-        );
+        return String.format("{\"pro_id\":\"%s\", \"pro_model\":\"%s\", \"pro_category\":\"%s\", " +
+                        "\"pro_name\":\"%s\", \"pro_current_price\":\"%.2f\", \"pro_raw_price\":\"%.2f\", " +
+                        "\"pro_discount\":\"%.2f\", \"pro_likes_count\":\"%d\"}",
+                proId, proModel, proCategory, proName, proCurrentPrice, proRawPrice, proDiscount, proLikesCount);
     }
 }
